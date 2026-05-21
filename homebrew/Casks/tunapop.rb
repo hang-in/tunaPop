bd41fe320c3af54b55ee5a2a9cbdc97b0668673e
d9ng <d9ng@outlook.com>
2026-05-22 05:09:25 +0900

    chore: pin cask sha256 for v0.1.0 release

--- homebrew/Casks/tunapop.rb
+++ homebrew/Casks/tunapop.rb
@@ -1,6 +1,6 @@
 cask "tunapop" do
   version "0.1.0"
-  sha256 :no_check # 첫 릴리즈 후 실제 DMG의 shasum 값으로 교체
+  sha256 "6cbff6ed0aa141ebf95702ab277003048c78500754b71d8dc60f03e0e85c9010"
 
   url "https://github.com/hang-in/tunapop/releases/download/v#{version}/tunaPop-#{version}.dmg"
   name "tunaPop"
