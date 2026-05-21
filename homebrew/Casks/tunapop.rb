10dfae9f684f39b243e9b6b9f45baa6058b3123d
d9ng <d9ng@outlook.com>
2026-05-22 06:34:46 +0900

    build: update tunapop Cask to v0.2.4

--- homebrew/Casks/tunapop.rb
+++ homebrew/Casks/tunapop.rb
@@ -1,6 +1,6 @@
 cask "tunapop" do
-  version "0.2.3"
-  sha256 "23a57844d827177c82cd4cf98b272181fa99a041bd9bc9848da7dd789029d3ae"
+  version "0.2.4"
+  sha256 "0964ce0df70f10d91d6a031fd56c4f25ac0eed086db6afae7a65d9f6ce42c04b"
 
   url "https://github.com/hang-in/tunapop/releases/download/v#{version}/tunaPop-#{version}.dmg"
   name "tunaPop"
